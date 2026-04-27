233264c50cfbfc39fcb7e481bdcce36e9c6dea06
marki1n-code <mramur47@gmail.com>
2026-04-27 11:02:52 +0300

    changes

--- script.js
+++ script.js
@@ -1,55 +1,55 @@
 let value;
 
 function exercise() {
-    
-switch (exercise) {
-    case "Chest":
-        images.push(`<img src="images/${exercise}.png" alt="Exercise ${exercise}">`); 
-        break;
-    
-    case "Forearms":
-        images.push(`<img src="images/${exercise}.png" alt="Exercise ${exercise}">`);
-        break;
 
-    case "Upper Back":
-        images.push(`<img src="images/${exercise}.png" alt="Exercise ${exercise}">`);        
-        break;
+    switch (exercise) {
+        case "Chest":
+            images.push(`<img src="images/${exercise}.png" alt="Exercise ${exercise}">`);
+            break;
 
-    case "Shoulders":
-        images.push(`<img src="images/${exercise}.png" alt="Exercise ${exercise}">`);        
-        break;
+        case "Forearms":
+            images.push(`<img src="images/${exercise}.png" alt="Exercise ${exercise}">`);
+            break;
 
-        images.push(`<img src="images/${exercise}.png" alt="Exercise ${exercise}">`);
-    case "Abdominals":
-        images.push(`<img src="images/${exercise}.png" alt="Exercise ${exercise}">`);        
-        break;
+        case "Upper Back":
+            images.push(`<img src="images/${exercise}.png" alt="Exercise ${exercise}">`);
+            break;
 
-        images.push(`<img src="images/${exercise}.png" alt="Exercise ${exercise}">`);
-    case "Biceps":
-        images.push(`<img src="images/${exercise}.png" alt="Exercise ${exercise}">`);        
-        break;
+        case "Shoulders":
+            images.push(`<img src="images/${exercise}.png" alt="Exercise ${exercise}">`);
+            break;
 
-        images.push(`<img src="images/${exercise}.png" alt="Exercise ${exercise}">`);
-    case "Triceps":
-        images.push(`<img src="images/${exercise}.png" alt="Exercise ${exercise}">`);        
-        break;
+            images.push(`<img src="images/${exercise}.png" alt="Exercise ${exercise}">`);
+        case "Abdominals":
+            images.push(`<img src="images/${exercise}.png" alt="Exercise ${exercise}">`);
+            break;
 
-        images.push(`<img src="images/${exercise}.png" alt="Exercise ${exercise}">`);
-    case "Calves":
-        images.push(`<img src="images/${exercise}.png" alt="Exercise ${exercise}">`);        
-        break;
+            images.push(`<img src="images/${exercise}.png" alt="Exercise ${exercise}">`);
+        case "Biceps":
+            images.push(`<img src="images/${exercise}.png" alt="Exercise ${exercise}">`);
+            break;
 
-        images.push(`<img src="images/${exercise}.png" alt="Exercise ${exercise}">`);
-    case "Glutes":
-        images.push(`<img src="images/${exercise}.png" alt="Exercise ${exercise}">`);        
-        break;
-        
+            images.push(`<img src="images/${exercise}.png" alt="Exercise ${exercise}">`);
+        case "Triceps":
+            images.push(`<img src="images/${exercise}.png" alt="Exercise ${exercise}">`);
+            break;
 
-    default:
+            images.push(`<img src="images/${exercise}.png" alt="Exercise ${exercise}">`);
+        case "Calves":
+            images.push(`<img src="images/${exercise}.png" alt="Exercise ${exercise}">`);
+            break;
 
-        console.log("Choose exrcise");
-        break;
-}
+            images.push(`<img src="images/${exercise}.png" alt="Exercise ${exercise}">`);
+        case "Glutes":
+            images.push(`<img src="images/${exercise}.png" alt="Exercise ${exercise}">`);
+            break;
+
+
+        default:
+
+            console.log("Choose exrcise");
+            break;
+    }
 }
 
 function exercise() {
